e7d09afa1aabbe8608fbcb7364e356a66bbae39d
Raden Ario Damar <ramadoiranedar@gmail.com>
2022-07-25 21:01:39 +0700

    add exam 4.1.string

---
+++ exam/4.1.string/invoice.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("-------------------------------")
+	fmt.Println("-- WELCOME TO THE BILLIARD 9 --")
+	fmt.Println("-------DATE 25 JULY 2022-------")
+	fmt.Println("-------------------------------")
+	fmt.Println("------------INVOICE------------")
+	fmt.Println("TABLE\t\t: IDR 200.000")
+	fmt.Println("TAX\t\t: IDR 20.000")
+	fmt.Println("-------------------------------")
+	fmt.Println("PAYMENT METHOD\t: CASH")
+	fmt.Println("PAYMENT\t\t: IDR 250.000")
+	fmt.Println("CHANGE\t\t: IDR 30.000")
+	fmt.Println("-------------------------------")
+}
