c3c2965d9d62aaf320e2f0c3bee16d646cc35952
Raden Ario Damar <ramadoiranedar@gmail.com>
2022-07-25 20:52:58 +0700

    add 4.1.string.go

--- 4.1.string.go
+++ 4.1.string.go
@@ -4,4 +4,8 @@ func main() {
 	fmt.Println("Raden")
 	fmt.Println("Raden Ario")
 	fmt.Println("Raden Ario Damar")
+
+	fmt.Println(len("Raden"))
+	fmt.Println("Raden Ario"[0])
+	fmt.Println("Raden Ario Damar"[1])
 }
