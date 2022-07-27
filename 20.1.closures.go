1d750dfd7c1f4d85591f62121c2eae1126f34e10
Raden Ario Damar <ramadoiranedar@gmail.com>
2022-07-27 23:13:50 +0700

    update closures

--- 20.1.closures.go
+++ 20.1.closures.go
@@ -6,13 +6,14 @@ func main() {
 	counter := 0 // global variable set only for 0
 	increment := func() {
 		fmt.Println("Increment")
-		counter := 0 // solution is declaration new variable
+		///counter := 0 // solution is declaration new variable for privating variable or closures
 		counter++ // reference global variable --> counter
-		fmt.Println("counter in func", counter) // variable local counter after changed
 
 	}
 
-	fmt.Println("counter global", counter) // variable global counter
+	fmt.Println("counter global", counter) // variable global counter before
 	increment()
 	increment()
+	fmt.Println("counter in func", counter) // variable global counter after
+
 }
